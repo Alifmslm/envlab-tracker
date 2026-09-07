bb6e7a26c70eaf16a7934e3b9eaa906c7a1d87da
Alifmslm <alifmslm01@gmail.com>
2026-09-07 17:03:28 +0700

    feat: create file data sampel (model, migration, controller)

---
+++ database/seeders/DataSampelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DataSampelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
